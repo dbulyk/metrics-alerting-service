bfc7b0ed961cbeed12296c3d35c7404933a01c3a
Dmitry <dbulyk@icloud.com>
2023-11-08 13:07:21 +0600

    The commit message provided seems incorrect for the diff. The diff indicates two files modified: `internal/utils/hash.go` and `cmd/agent/internal/services/metricService.go`. In the first file, a log statement was added to print the inputs of the Hash function. In the second file, two similar log statements were removed. There is no delay introduced, and the file `cmd/agent/agent.go` is not part of the changes. Here's a possible correct commit message:
    Implement logging improvements in hash functions and metrics service
    
    Integrated improved logging mechanisms into hashing and metrics service. Specifically, logs were added to the hash function to note when it is being called and print its inputs (`internal/utils/hash.go`). Additionally, unnecessary logging in the metrics service was removed, reducing noise in the log file (`cmd/agent/internal/services/metricService.go`). These changes aim to improve tracking and debugging.

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -4,9 +4,11 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
+	"github.com/rs/zerolog/log"
 )
 
 func Hash(message, key string) string {
+	log.Info().Msgf("message: %s, key: %s", message, key)
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(message))
 	return fmt.Sprintf("%x", h.Sum(nil))
