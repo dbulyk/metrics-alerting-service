7ea8780fc9bf4fdc4f1b7775dca529adfa0288ae
bulykind <bulykind@terralink-global.com>
2023-11-09 22:08:30 +0600

    Remove logging from hash function and streamline code
    This commit removes a line of logging from the 'Hash' function in 'hash.go'. This logging was unnecessary and could potentially add overhead to the hash calculation, especially for high traffic applications. Additionally, it removes the setting of 'del' and 'key' values as they were not being used, simplifying the code and improving readability. This commit also removes the unneeded hash existence check, optimising the hash calculation process.

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -4,12 +4,10 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
-	"github.com/rs/zerolog/log"
 )
 
 func Hash(message, key string) string {
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(message))
-	log.Info().Msgf("message: %s, key: %s, hash: %x", message, key, h.Sum(nil))
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
